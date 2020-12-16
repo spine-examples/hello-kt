a6af502c793daaf93f8394884e0458afa4d7a80e
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2020-12-16 23:02:10 +0200

    Add idea plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.21"
+    idea
     id("io.spine.tools.gradle.bootstrap") version "1.7.0"
 }
 
-val javaVersion = "1.8"
+val jvmVersion = "1.8"
 
 spine.enableJava().server()
 
@@ -13,16 +14,12 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
 }
 
-repositories {
-    jcenter()
-}
-
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
-    jvmTarget = javaVersion
+    jvmTarget = jvmVersion
 }
 
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
-    jvmTarget = javaVersion
+    jvmTarget = jvmVersion
 }
